0f14f32249e311c82ddd8cb9e1f1641ef9299243
Alfred Lau <alfred@sulphur.com.sg>
2022-09-27 03:19:21 +0800

    :clown_face: the mocking continues

--- src/data/collections.ts
+++ src/data/collections.ts
@@ -41,7 +41,7 @@ const collections: Collection[] = [
   },
   {
     title: "Storm of Swords",
-    status: "CurrentlyMinting",
+    status: "FinishedMinting",
     slug: "abstract-odyssey-3",
     coverImg:
       "https://images.unsplash.com/photo-1580610447943-1bfbef5efe07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
