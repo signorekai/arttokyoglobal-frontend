cd774fbd2c02f0792fef3213c6d3069ce8ef0771
Alfred Lau <alfred@sulphur.com.sg>
2022-09-27 23:20:28 +0800

    :clown_face: mock artists data

--- src/data/artists.ts
+++ src/data/artists.ts
@@ -8,34 +8,42 @@ const artists: Artist[] = [
   {
     name: 'Rachael Goto',
     slug: 'rachael-goto',
+    profilePhoto: 'https://source.unsplash.com/500x500?portrait'
   },
   {
     name: 'Elizabeth Li',
     slug: 'elizabeth-li',
+    profilePhoto: 'https://source.unsplash.com/500x500?portrait'
   },
   {
     name: 'Liu Bei',
     slug: 'liu-bei',
+    profilePhoto: 'https://source.unsplash.com/500x500?portrait'
   },
   {
     name: 'Mark Gomez',
     slug: 'mark-gomez',
+    profilePhoto: 'https://source.unsplash.com/500x500?portrait'
   },
   {
     name: 'George R',
-    slug: 'george-r'
+    slug: 'george-r',
+    profilePhoto: 'https://source.unsplash.com/500x500?portrait'
   },
   {
     name: '#1xb0e2',
     slug: '1xb0e2',
+    profilePhoto: 'https://source.unsplash.com/500x500?portrait'
   },
   {
     name: 'Robin T',
-    slug: 'robin-t'
+    slug: 'robin-t',
+    profilePhoto: 'https://source.unsplash.com/500x500?portrait'
   },
   {
     name: "Michelle Yeo",
-    slug: 'michelle-yeo'
+    slug: 'michelle-yeo',
+    profilePhoto: 'https://source.unsplash.com/500x500?portrait'
   }
 ]
 
