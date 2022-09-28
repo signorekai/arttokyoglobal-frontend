b2b9932d650d2c5c097191cf429ec0e0b1a3914c
Alfred Lau <alfred@sulphur.com.sg>
2022-09-28 17:53:18 +0800

    :wrench: some SSR required

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import alpine from "@astrojs/alpinejs";
-import nesting from 'tailwindcss/nesting'
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+import nesting from 'tailwindcss/nesting';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [alpine(), tailwind({
     plugins: [nesting],
     config: {
       applyBaseStyles: false
     }
-  })]
+  })],
+  adapter: vercel()
 });
