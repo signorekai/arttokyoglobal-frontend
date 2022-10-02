266769e670720615576283b3baa65cf2e0268e24
Alfred Lau <alfred@sulphur.com.sg>
2022-10-03 01:09:24 +0800

    :tada:

---
+++ src/data/faq.ts
@@ -0,0 +1,33 @@
+const sections = [{
+  title: "Section 1",
+  item: [
+    { 
+      header: "Who is john galt?",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore expedita id tempore porro nesciunt corporis fugiat ipsa quis dolores, odit sed libero quo quisquam. Nisi, dignissimos rem? Sit, impedit! Sed, aspernatur ab.",
+    },
+    { 
+      header: "Question 2",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, quas!",
+    },
+
+  ]
+},
+{
+  title: "Section 2",
+  item: [
+    { 
+      header: "Question 3",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore expedita id tempore porro nesciunt corporis fugiat ipsa quis dolores, odit sed libero quo quisquam. Nisi, dignissimos rem? Sit, impedit! Sed, aspernatur ab.",
+    },
+    { 
+      header: "Question 4",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, quas!",
+    },
+    { 
+      header: "Question 5",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, quas!",
+    },
+  ]
+}];
+
+export default sections;
