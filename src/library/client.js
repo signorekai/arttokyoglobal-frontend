e198f4d5c11588f15b19a3e12c240ef3ed07dbcc
Alfred Lau <alfred@sulphur.com.sg>
2022-10-18 19:52:21 +0800

    :recycle: refactor code to cater for image

--- src/library/client.js
+++ src/library/client.js
@@ -1,4 +1,5 @@
 import { BigNumber } from "ethers";
+import { getLargestPossibleImage } from "./helpers";
 
 export const getPrice = ({
   startDate,
@@ -28,4 +29,41 @@ export const getPrice = ({
   }
 
   return price
+}
+
+export function imageHelper (imageObj, maxSize, aspectRatio = false) {
+  return {
+    checkPrefix(string) {
+      return string.slice(0, 5) !== 'https' ? `https://${string}` : string;
+    },
+    image: {
+      ["x-bind:src"]() {
+        return this.checkPrefix(getLargestPossibleImage(imageObj, maxSize));
+      },
+      ["x-bind:style"]() {
+        const styles = {}
+
+        if (aspectRatio) {
+          styles["aspect-ratio"] = `${imageObj.width} / ${imageObj.height}`
+        }
+
+        return styles
+      }
+    },
+    backgroundImage: {
+      ["x-bind:style"]() {
+        let image = this.checkPrefix(getLargestPossibleImage(imageObj, maxSize));
+
+        const styles = {
+          "background-image": `url(${image})`
+        }
+
+        if (aspectRatio) {
+          styles["aspect-ratio"] = `${imageObj.width} / ${imageObj.height}`
+        }
+
+        return styles
+      }
+    }
+  }
 }
