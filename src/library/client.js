eab9e70ac2be136aec68d9c39721a3a9a62ffecb
Alfred Lau <alfred@sulphur.com.sg>
2022-10-18 20:22:30 +0800

    :sparkles: no dutch auction

--- src/library/client.js
+++ src/library/client.js
@@ -2,32 +2,10 @@ import { BigNumber } from "ethers";
 import { getLargestPossibleImage } from "./helpers";
 
 export const getPrice = ({
-  startDate,
-  dynamicStart,
   mintPrice,
 }) => {
-  var price;
-
-  const bigStartDate = BigNumber.from(startDate);
   const bigMintPrice = BigNumber.from(mintPrice);
-
-  const dateNow = Date.now();
-  if (bigStartDate.gt(Math.floor(dateNow/1000)) && (dynamicStart === true) ) {
-    price = bigMintPrice;
-    console.log("[Dapp] Current Token Price1:", price);
-  }
-  else if ((bigStartDate.add(3600)).gt(Math.floor(dateNow/1000)) && (dynamicStart === true) ) {
-    const multiplier = ((bigStartDate.add(3600)).sub(Math.floor(dateNow/1000))).div(60)
-    price = bigMintPrice.mul(multiplier)
-
-    console.log("[Dapp] Current Token Price2:", price);
-    console.log("[Dapp] Current Multiplier:", multiplier);
-  }
-  else {
-    price = bigMintPrice;
-    console.log("[Dapp] Current Token Price3:", price);
-  }
-
+  let price = bigMintPrice;
   return price
 }
 
