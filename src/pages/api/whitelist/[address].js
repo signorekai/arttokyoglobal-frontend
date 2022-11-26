232eb9df935e907e7a847c980af58b0fdbc939a2
Alfred Lau <alfred@sulphur.com.sg>
2022-11-27 03:30:20 +0800

    :zap: fetch from cms again

--- src/pages/api/whitelist/[address].js
+++ src/pages/api/whitelist/[address].js
@@ -1,11 +1,13 @@
 import Whitelist from '../../../library/whitelist';
+import masterWhitelist from '../../../whitelist.json';
 
 export async function post({params, request}) {
   if (request.headers.get("Content-Type") === 'application/json') {
     const body = await request.json();
     const { address } = params;
-    const { whitelistedAddresses } = body;
+    const { slug } = body;
 
+    const whitelistedAddresses = masterWhitelist.hasOwnProperty(slug) ? masterWhitelist[slug] : [];
     const whitelist = new Whitelist(whitelistedAddresses);
 
     return new Response(JSON.stringify({
