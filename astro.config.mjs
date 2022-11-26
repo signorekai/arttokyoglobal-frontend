874daf2ce92e2aed8615075d8c197e35c2cec461
Alfred Lau <alfred@sulphur.com.sg>
2022-11-27 00:57:20 +0800

    :wrench: use netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,18 @@ import alpine from "@astrojs/alpinejs";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import nesting from 'tailwindcss/nesting';
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
   integrations: [alpine(), tailwind({
     plugins: [nesting],
     config: {
       applyBaseStyles: false
     }
-  }), mdx()] 
-
+  }), mdx()]
 });
