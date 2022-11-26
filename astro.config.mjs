4fe87fe93d31060040e3d6b5455a17737f7889a1
Alfred Lau <alfred@sulphur.com.sg>
2022-11-27 01:39:55 +0800

    :wrench: needs to be SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [alpine(), tailwind({
     plugins: [nesting],
     config: {
