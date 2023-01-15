01178788e6063e80c9f8827c5ad1d849638da603
Alfred Lau <alfred@sulphur.com.sg>
2023-01-15 19:39:24 +0800

    :sparkles: move whitelist checking to CMS

--- src/pages/api/whitelist/[address].js
+++ src/pages/api/whitelist/[address].js
@@ -3,17 +3,22 @@ import masterWhitelist from '../../../whitelist.json';
 
 export async function post({params, request}) {
   if (request.headers.get("Content-Type") === 'application/json') {
+    
     const body = await request.json();
     const { address } = params;
-    const { slug } = body;
+    const { slug, id } = body;
 
-    const whitelistedAddresses = masterWhitelist.hasOwnProperty(slug) ? masterWhitelist[slug] : [];
-    const whitelist = new Whitelist(whitelistedAddresses);
+    const response = await fetch(`${import.meta.env.STRAPI_API}/api/collections/${id}/whitelist/${address}`, {
+    })
 
-    return new Response(JSON.stringify({
-      isUserWhitelisted: whitelist.contains(address),
-      whitelistProof: whitelist.getProofForAddress(address),
-    }))
+    const result = await response.json()
+
+    return new Response(JSON.stringify(result), {
+      status: response.status,
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
   }
   return new Response(null, {status: 400})
 }
