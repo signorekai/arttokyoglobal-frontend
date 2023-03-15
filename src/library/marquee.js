e23b9eb8ded252b47dd0e5ae22345eaec9223c36
Alfred Lau <alfred@sulphur.com.sg>
2023-03-15 23:57:16 +0800

    :lipstick: added floating displays of tokens

--- src/library/marquee.js
+++ src/library/marquee.js
@@ -20,7 +20,7 @@ export function marquee({tokens, totalAmount = 30, defaultImage = null}) {
 
         this.selectedTokens.push(token);
         const left = Math.floor(Math.random() * 100);
-        const elem = `<div class="${left <= 50 ? 'animate-[marquee-right_50s_linear]' : 'animate-[marquee-left_50s_linear]' } cursor-pointer hover:opacity-40 transition-opacity absolute shadow-card bg-white opacity-20 rounded-xl" style="animation-duration: ${Math.random() * 300 + 100}s; left: ${left}%; top: ${Math.floor(Math.random()*100)}%; width: ${Math.ceil(Math.random() * 2) + 2}rem; aspect-ratio: 4 / 3"></div>`;
+        const elem = `<div class="${left <= 50 ? 'animate-[marquee-right_50s_linear]' : 'animate-[marquee-left_50s_linear]' } cursor-pointer hover:opacity-40 transition-opacity absolute shadow-card opacity-20 rounded-xl bg-contain" style="background-image: url(${getLargestPossibleImage(token.image, 'thumbnail')}); animation-duration: ${Math.random() * 300 + 100}s; left: ${left}%; top: ${Math.floor(Math.random()*100)}%; width: ${Math.ceil(Math.random() * 2) + 2}rem; aspect-ratio: 1 / 1"></div>`;
         const el = generateElements(elem)
         el[0].addEventListener('click', () => {
           this.changeToken(token);
